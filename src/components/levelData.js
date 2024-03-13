e60312d1b2f37d959c2f488282a70c2c31b2840a
cuthere01 <ufa.timurah@gmail.com>
2024-03-13 15:26:55 +0300

    timer fixes

--- src/components/levelData.js
+++ src/components/levelData.js
@@ -7,31 +7,31 @@ export default{
             "number": 1,
             "dices": 4,
             "cols": 2,
-            "time": 3
+            "time": 5
         },
         {
             "number": 2,
             "dices": 6,
             "cols": 3,
-            "time": 3
+            "time": 5
         },
         {
             "number": 3,
             "dices": 9,
             "cols": 3,
-            "time": 3
+            "time": 10
         },
         {
             "number": 4,
             "dices": 12,
             "cols": 4,
-            "time": 3
+            "time": 10
         },
         {
             "number": 5,
             "dices": 16,
             "cols": 4,
-            "time": 3
+            "time": 15
         },
     ]
 }
